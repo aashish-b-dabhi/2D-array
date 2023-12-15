dc343290055889ee8568e4b47f1e1d305df05f46
DABHI AASHISH BHARATBHAI <150463240+aashish-b-dabhi@users.noreply.github.com>
2023-12-15 22:05:18 +0530

    Add files via upload

---
+++ Column-Sum.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+main()
+{
+	int r,c,i,j;
+	
+	printf("Size of Row : ");
+	scanf("%d",&r);
+	
+	printf("Size of Column : ");
+	scanf("%d",&c);
+	
+ 	int a[r][c];
+	
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+		printf("\n");
+	}
+	
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			printf("%d ",a[i][j]);
+		}
+		printf("\n");
+	}
+	for(i=0;i<r;i++)
+	{
+		int sum=0;
+		
+		for(j=0;j<c;j++)
+		{
+			sum = sum + a[j][i];
+		}
+		printf("Sum Of Column = %d",sum);
+		printf("\n");
+	}
+
+}
---
+++ Cross-Daigonal-Sum.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+
+main()
+{
+	int i,j,r,c,sum=0,sum1=0;
+	
+	printf("Size of Row = ");
+	scanf("%d",&r);
+	
+	printf("Size of Column = ");
+	scanf("%d",&c);
+	
+	int a[r][c];
+	
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			scanf("%d",&a[i][j]);
+		}
+		printf("\n");
+	}
+	
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			printf("%d ",a[i][j]);
+		}
+		printf("\n");
+	}
+	
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			if(i==j)
+			{
+				sum = sum + a[i][j];
+			}
+			
+			if(i+j==2)	 
+			{
+				sum1 = sum1 + a[i][j];				
+			}
+		}
+	}
+	
+	int sum3 = sum + sum1;
+	
+	printf("\n");
+	
+	printf("Daigonal Sum = %d \n",sum);
+	
+	printf("Anti-Daigonal Sum = %d \n",sum1);
+	
+	printf("Cross-Daigonal Sum = %d ",sum3);
+	
+}
